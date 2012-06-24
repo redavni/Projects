01178c5cf8c8a93203979e34ca70ddd8a06eb05f
Jake Klassen <klassenja@hotmail.com>
2012-06-23 22:14:54 -0400

    Did some basic class and GUI work

---
+++ notes_gui/Noted/Classes/Note.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Noted.Classes
+{
+    public class Note
+    {
+        #region Private Members
+
+        private string _title;
+        private string _body;
+        
+        // TODO At Some point implement storage of styling sheets
+
+        private DateTime _dateCreated;
+        private DateTime _lastModified;
+
+        #endregion
+
+        #region Properties
+
+        public string Title
+        {
+            get { return _title; }
+            set { _title = value; }
+        }
+
+        public string Body
+        {
+            get { return _body; }
+            set { _body = value; }
+        }
+
+        public DateTime DateCreated
+        {
+            get { return _dateCreated; }
+            set { _dateCreated = value; }
+        }
+
+        public DateTime LastModified
+        {
+            get { return _lastModified; }
+            set { _lastModified = value; }
+        }
+
+        #endregion
+
+        #region Methods
+
+        #endregion
+
+    }
+}
---
+++ notes_gui/Noted/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Noted
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            TabItem ti = new TabItem() { Header = "New File" };
+            TextBox tb = new TextBox() { AcceptsReturn = true, AcceptsTab = true, VerticalScrollBarVisibility = ScrollBarVisibility.Auto, 
+                                         TextWrapping = TextWrapping.Wrap };
+            ti.Content = tb;
+            tabControlFiles.Items.Add(ti);
+            tabControlFiles.SelectedItem = ti;
+
+            //labelStatus.Content = Convert.ToString(RenderCapability.Tier >> 16);
+        }
+
+        private void MenuItemExit_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.MainWindow.Close();
+        }
+    }
+}
